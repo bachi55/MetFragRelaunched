da3eb12723306e0edc0ea16098ed4f1ba3c1671a
Eric Bach <eric.bach@aalto.fi>
2018-07-23 12:20:53 +0300

    add more tests: query by mass + ppm

--- MetFragLib/src/test/java/de/ipbhalle/metfraglib/LocalPubChemSQLite_Test.java
+++ MetFragLib/src/test/java/de/ipbhalle/metfraglib/LocalPubChemSQLite_Test.java
@@ -7,33 +7,25 @@ import de.ipbhalle.metfraglib.process.CombinedMetFragProcess;
 import de.ipbhalle.metfraglib.settings.MetFragGlobalSettings;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
+import de.ipbhalle.metfraglib.settings.Settings;
 import org.junit.Before;
 import org.junit.Test;
 import org.apache.log4j.Logger;
 import java.io.File;
 import java.io.IOException;
-import java.nio.DoubleBuffer;
 
 public class LocalPubChemSQLite_Test {
     private Logger logger;
-    private String finalParameterFilePath;
+    private String paramFilePath_by_identifier;
+    private String paramFilePath_by_monomass;
+    private String paramFilePath_by_identifier_cid233;
 
-    @Before
-    public void setUp() {
-        this.logger = Logger.getLogger(LocalPubChemSQLite_Test.class);
-        java.io.File parameterFilePath = new java.io.File(ClassLoader.getSystemResource("local_sqlite_example_pubchemcid.txt").getFile());
-        String peakListFilePath = ClassLoader.getSystemResource("peaklist_file_example_1.txt").getFile();
-        String pubchemDBFilePath = ClassLoader.getSystemResource("pubchem.db").getFile();
-        /*
-         * read file parameters and add additional ones into a new temporary parameter file
-         * - peaklist file location
-         * - temp folder as result storage
-         */
-        String tempDir = System.getProperty("java.io.tmpdir");
+    private String get_absolute_path_of_temporary_parameter_file(java.io.File paramFile, String tempDir) {
         java.io.File tempFile = null;
         try {
-            tempFile = java.io.File.createTempFile("temp-file-name", ".tmp");
+            tempFile = java.io.File.createTempFile(paramFile.getName().split("[.]")[0], null);
             tempFile.deleteOnExit();
         } catch (IOException e) {
             e.printStackTrace();
@@ -41,10 +33,11 @@ public class LocalPubChemSQLite_Test {
         if(tempFile == null) {
             throw new NullPointerException();
         }
+
         java.io.BufferedWriter bwriter;
         try {
             bwriter = new java.io.BufferedWriter(new java.io.FileWriter(tempFile));
-            java.io.BufferedReader breader = new java.io.BufferedReader(new java.io.FileReader(parameterFilePath));
+            java.io.BufferedReader breader = new java.io.BufferedReader(new java.io.FileReader(paramFile));
             String line = "";
             while((line = breader.readLine()) != null) {
                 line = line.trim();
@@ -58,32 +51,27 @@ public class LocalPubChemSQLite_Test {
             bwriter.write("ResultsPath = " + tempDir);
             bwriter.newLine();
             /*
-             * add peaklist path
+             * add dummy peaklist path
              */
-            bwriter.write("PeakListPath = " + peakListFilePath);
+            bwriter.write("PeakListPath = " + ClassLoader.getSystemResource("peaklist_file_example_1.txt").getFile());
             bwriter.newLine();
 
             /*
              * add pubchem database path
              */
-            bwriter.write("LocalPubChemDatabase = " + pubchemDBFilePath);
+            bwriter.write("LocalPubChemDatabase = " + ClassLoader.getSystemResource("pubchem.db").getFile());
             bwriter.newLine();
             bwriter.close();
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-        this.finalParameterFilePath = tempFile.getAbsolutePath();
+
+        return(tempFile.getAbsolutePath());
     }
 
-    @Test
-    public void test_query_by_monomass() {
-        // TODO: Implement by comparison with the online pubchem db class.
-    }
-
-    @Test
-    public void test_query_by_identifier() {
-        File parameterFile = new File(this.finalParameterFilePath);
+    private MetFragGlobalSettings load_settings(String paramFilePath) {
+        File parameterFile = new File(paramFilePath);
 
         MetFragGlobalSettings settings = null;
         try {
@@ -95,7 +83,12 @@ public class LocalPubChemSQLite_Test {
         }
 
         SettingsChecker settingsChecker = new SettingsChecker();
-        if(!settingsChecker.check(settings)) return;
+        if(!settingsChecker.check(settings)) return null;
+
+        return(settings);
+    }
+
+    private CandidateList get_candidate_list_for_setting(MetFragGlobalSettings settings) {
         //init the MetFrag process
         CombinedMetFragProcess mp = new CombinedMetFragProcess(settings);
 
@@ -109,12 +102,89 @@ public class LocalPubChemSQLite_Test {
             System.exit(2);
         }
 
-        //fetch the scored candidate list
-        CandidateList candidateList = mp.getCandidateList();
+        return(mp.getCandidateList());
+    }
+
+    @Before
+    public void setUp() {
+        this.logger = Logger.getLogger(LocalPubChemSQLite_Test.class);
+
+        java.io.File paramFile_by_identifier = new java.io.File(ClassLoader.getSystemResource("local_sqlite_example_pubchemcid.txt").getFile());
+        java.io.File paramFile_by_monomass = new java.io.File(ClassLoader.getSystemResource("local_sqlite_example_monoisotopicmass.txt").getFile());
+        java.io.File paramFile_by_identifier_cid233 = new java.io.File(ClassLoader.getSystemResource("local_sqlite_example_cid233.txt").getFile());
+
+        String tempDir = System.getProperty("java.io.tmpdir");
+
+        this.paramFilePath_by_identifier = get_absolute_path_of_temporary_parameter_file(paramFile_by_identifier, tempDir);
+        this.paramFilePath_by_monomass = get_absolute_path_of_temporary_parameter_file(paramFile_by_monomass, tempDir);
+        this.paramFilePath_by_identifier_cid233 = get_absolute_path_of_temporary_parameter_file(paramFile_by_identifier_cid233, tempDir);
+    }
+
+    @Test
+    public void test_NULL_handling() {
+        // pubchem compound with cid = 233 has no xlogp3 ==> return null when querying it.
+        MetFragGlobalSettings settings = load_settings(this.paramFilePath_by_identifier_cid233);
+        assertNotNull(settings);
+    }
+
+    @Test
+    public void test_query_by_monomass() {
+        // load the settings
+        MetFragGlobalSettings settings = load_settings(this.paramFilePath_by_monomass);
+        assertNotNull(settings);
+
+        // query result cids
+        String[] cids = new String[9];
+        cids[0] = "9";
+        cids[1] = "208";
+        cids[2] = "466";
+        cids[3] = "481";
+        cids[4] = "603";
+        cids[5] = "717";
+        cids[6] = "719";
+        cids[7] = "5958";
+        cids[8] = "17426";
+
+        // First load only candidate with exactly the same mass
+        settings.set(VariableNames.DATABASE_RELATIVE_MASS_DEVIATION_NAME, 0.0);
+
+        //fetch the candidate list
+        CandidateList candidateList = get_candidate_list_for_setting(settings);
+        assertEquals(candidateList.getNumberElements(), 8);
+
+        for (int i = 0; i < candidateList.getNumberElements(); i++) {
+            assertEquals(cids[i], candidateList.getElement(i).getIdentifier());
+        }
+
+        // Load candidate with a small mass deviation
+        settings.set(VariableNames.DATABASE_RELATIVE_MASS_DEVIATION_NAME, 5.0);
+
+
+        //fetch the candidate list
+        candidateList = get_candidate_list_for_setting(settings);
+        assertEquals(candidateList.getNumberElements(), 9);
+
+        for (int i = 0; i < candidateList.getNumberElements(); i++) {
+            assertEquals(cids[i], candidateList.getElement(i).getIdentifier());
+        }
+    }
+
+    @Test
+    public void test_query_by_identifier() {
+        // load the settings
+        MetFragGlobalSettings settings = load_settings(this.paramFilePath_by_identifier);
+        assertNotNull(settings);
+
+        // fetch the candidate list
+        CandidateList candidateList = get_candidate_list_for_setting(settings);
 
         // Check that all 4 candidates could be retrieved (requires that they are all available in the
         // sandbox db!) CIDS used here: 1,3,9,245
-        String[] cids = (String[]) settings.get(VariableNames.PRECURSOR_DATABASE_IDS_NAME);
+        String[] cids = new String[4];
+        cids[0] = "1";
+        cids[1] = "3";
+        cids[2] = "9";
+        cids[3] = "245";
 
         String[] inchis = new String[cids.length];
         inchis[0] = "InChI=1S/C9H17NO4/c1-7(11)14-8(5-9(12)13)6-10(2,3)4/h8H,5-6H2,1-4H3";
